521e93516c946abe2c7ea078a6e9ba293516ea0b
leeronghui <lironghui@126.com>
2019-09-23 16:22:50 +0800

    modified test.c

---
+++ test.c
@@ -0,0 +1,16 @@
+/*************************************************************************
+	#	 FileName	: test.c
+	#	 Author		: lironghui 
+	#	 Email		: lironghui@126.com 
+	#	 Created	: Mon 23 Sep 2019 04:17:42 PM CST
+ ************************************************************************/
+
+#include<stdio.h>
+
+int main(int argc, const char *argv[])
+{
+	printf("hello world!\n");
+	printf("hello linux.\n");
+	
+	return 0;
+}
